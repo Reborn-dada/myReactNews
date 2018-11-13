b28a05f0077484bd95d7e68c34c007cb7926d5fb
lx <1224662297@qq.com>
2018-11-13 16:51:02 +0800

    update pc_usercenter.js

--- src/js/components/pc_usercenter.js
+++ src/js/components/pc_usercenter.js
@@ -1,124 +1,135 @@
 import React from 'react';
-import {Row, Col} from 'antd';
+import ReactDOM from 'react-dom';
+import {Row, Col, Modal} from 'antd';
+import {Menu, Icon} from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 import {
-	Menu,
-	Icon,
 	Tabs,
 	message,
 	Form,
 	Input,
 	Button,
-	CheckBox,
-	Modal,
+	Checkbox,
 	Card,
-	notification,   
-    Upload
+	notification,
+	Upload
 } from 'antd';
 const FormItem = Form.Item;
-const SubMenu = Menu.SubMenu;
 const TabPane = Tabs.TabPane;
-const MenuItemGroup = Menu.ItemGroup;
-
-import Header from './pc_header'
-import Footer from './pc_footer'
 import {Router, Route, Link, browserHistory} from 'react-router'
+import PCHeader from './pc_header';
+import PCFooter from './pc_footer';
+export default class PCUserCenter extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			usercollection: '',
+			usercomments: '',
+			previewImage: '',
+			previewVisible: false
+		};
+	};
+	componentDidMount() {
+		var myFetchOptions = {
+			method: 'GET'
+		};
 
-export default class PCUserCenter extends React.Component{
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getuc&userid=" + localStorage.userid, myFetchOptions)
+		.then(response=>response.json())
+		.then(json=>{
+			this.setState({usercollection:json});
+		});
 
-    constructor(){
-        super();
-        this.state={
-            previewVisible:false,
-            previewImage:'',
-            usercollection:''
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getusercomments&userid=" + localStorage.userid, myFetchOptions)
+		.then(response=>response.json())
+		.then(json=>{
+			this.setState({usercomments:json});
+		});
 
-        }
-    }
+	};
+	render() {
+		const props = {
+			action: 'http://newsapi.gugujiankong.com/handler.ashx',
+			headers: {
+				"Access-Control-Allow-Origin": "*"
+			},
+			listType: 'picture-card',
+			defaultFileList: [
+				{
+					uid: -1,
+					name: 'xxx.png',
+					state: 'done',
+					url: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png',
+					thumbUrl: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png'
+				}
+			],
+			onPreview: (file) => {
+				this.setState({previewImage: file.url, previewVisible: true});
+			}
+		};
 
-    
-    componentDidMount(){
-        var myFetchOptions = {
-            method:'GET'
-        }
-        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getuc&userid="+localStorage.userid,myFetchOptions)
-        .then(response=>response.json())
-        .then(json=>{
-            this.setState({userCollection:json})
-        });
-    };
+		const {usercollection,usercomments} = this.state;
+		const usercollectionList = usercollection.length ?
+		usercollection.map((uc,index)=>(
+				<Card key={index} title={uc.uniquekey} extra={<a target="_blank" href={`/#/details/${uc.uniquekey}`}>查看</a>}>
+					<p>{uc.Title}</p>
+				</Card>
+		))
+		:
+		'您还没有收藏任何的新闻，快去收藏一些新闻吧。';
 
-    render(){
+		const usercommentsList = usercomments.length ?
+		usercomments.map((comment,index)=>(
+				<Card key={index} title={`于 ${comment.datetime} 评论了文章 ${comment.uniquekey}`} extra={<a target="_blank" href={`/#/details/${comment.uniquekey}`}>查看</a>}>
+					<p>{comment.Comments}</p>
+				</Card>
+		))
+		:
+		'您还没有发表过任何评论。';
 
-        const props = {
-
-            action:'http://newsapi.gugujiankong.com/handler.ashx',
-            handle:{
-                "Access-Control-Allow-Origin":"*"
-            },
-            listType:"picture-card",
-            defaultFileList: [{
-                uid: '-1',
-                name: 'xxx.png',
-                status: 'done',
-                url:'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-			    thumbUrl:'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
-            }],
-            onPreview:(file)=>{
-                this.setState={
-                    previewVisble:true,
-                    previewImage:file.url
-                }
-            }
-        };
-
-        const {usercollection} = this.state;
-        const usercollectionList = usercollection.length
-        ?
-        usercollection.map((uc,index)=>(
-            <Card key={index} title={uc.uniquekey} extra={<a target='_blank' href={`/#/details/${uc.uniquekey}`}>查看</a>}>
-                <p>{uc.Title}</p>
-            </Card>
-        ))
-        :
-        '没有加载到收藏文章，请去收藏！';
-
-
-        
-        return(
-            <div>
-                <Header></Header>
-                <Row>
-                    <Col span={2}></Col>
-                    <Col span={20}>
-                        <Tabs>
-                            <TabPane tab="我的收藏" key="1">
-                                <div className="comment">
-                                    <Row span={24}>
-                                        {usercollectionList}                           
-                                    </Row>                 
-                                </div> 
-                            </TabPane>
-                            <TabPane tab="我的评价" key="2">
-                                
-                            </TabPane>
-                            <TabPane tab="个人头像" key="3">
-                                <Upload {...props}>
-                                    <Icon type='plus'></Icon>
-                                    <div className='ant-upload-text'>上传照片</div>
-                                </Upload>
-                                <Modal visible={this.state.previewVisible} footer={null} onCancel={this.handleCancel}>
-                                    <img alt='预览' src={this.state.previewImage}></img>
-                                </Modal>
-                                
-                            </TabPane>
-                        </Tabs>
-                    </Col>
-                    <Col span={2}></Col>
-                </Row>
-                <Footer></Footer>
-            </div>
-
-        )
-    }
-
+		return (
+			<div>
+				<PCHeader/>
+				<Row>
+					<Col span={2}></Col>
+					<Col span={20}>
+						<Tabs>
+							<TabPane tab="我的收藏列表" key="1">
+								<div class="comment">
+									<Row>
+										<Col span={24}>
+											{usercollectionList}
+										</Col>
+									</Row>
+								</div>
+							</TabPane>
+							<TabPane tab="我的评论列表" key="2">
+							<div class="comment">
+								<Row>
+									<Col span={24}>
+										{usercommentsList}
+									</Col>
+								</Row>
+							</div>
+							</TabPane>
+							<TabPane tab="头像设置" key="3">
+								<div class="clearfix">
+									<Upload {...props}>
+										<Icon type="plus"/>
+										<div className="ant-upload-text">上传照片</div>
+									</Upload>
+									<Modal visible ={this.state.previewVisible} footer={null} onCancel={this.handleCancel}>
+										<img alt="预览" src={this.state.previewImage}/>
+									</Modal>
+								</div>
+							</TabPane>
+						</Tabs>
+					</Col>
+					<Col span={2}></Col>
+				</Row>
+				<PCFooter/>
+			</div>
+		);
+	};
 }
